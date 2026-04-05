70158fd3de70498885ab0c30126f1fd466a47deb
Nikita Shcherbak~ <shernik112@gmail.com>
2026-04-05 19:44:03 +0300

    ScoreMvc

--- Assets/Scripts/UI/EntryPointMvc.cs
+++ Assets/Scripts/UI/EntryPointMvc.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Project.Player;
 using UnityEngine;
 using Zenject;
@@ -19,7 +20,7 @@ namespace Project.UI
             PlayerController = playerController;
         }
 
-        private void Awake()
+        protected virtual void Awake()
         {
             _view = GetComponent<MvcView>();
             PlayerController.OnHitPlayer += ResetCount;
@@ -41,6 +42,50 @@ namespace Project.UI
 
         private void ResetCount() =>
             Model.Reset();
+    }
+
+    public class Score : EntryPointMvc
+    {
+        private HandlerScore _handlerScore;
+        private readonly float _counterSpeed = 700;
+        private Coroutine _currentCoroutine;
+        
+        [Inject]
+        public void Init(HandlerScore handlerScore)
+        {
+            _handlerScore = handlerScore;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _handlerScore.NewTargetScore += Model.SetCount;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            _handlerScore.NewTargetScore -= Model.SetCount;
+        }
+        
+        private void StartCounterNewChange(int targetScore)
+        {
+            if (_currentCoroutine != null)
+                StopCoroutine(_currentCoroutine);
+            
+            _currentCoroutine = StartCoroutine(CounterNewChange(targetScore));
+        }
+        
+        private IEnumerator CounterNewChange(int targetScore)
+        {
+            while (Model.Count != targetScore)
+            {
+                var value = Mathf.RoundToInt(Mathf.MoveTowards(Model.Count, targetScore, _counterSpeed * Time.deltaTime));
+                Model.SetCount(value);
+                yield return null;
+            }
+            _currentCoroutine = null;
+        }
     }
 }
 
